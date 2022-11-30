b3573df3a21d0d7ef392dbade11ee9ff75a3594f
Yuan Cao <yuancao85@gmail.com>
2022-11-30 01:56:42 -0500

    create models for the important database objects to be POJOs/relatively dumb structs

--- src/main/java/org/revature/p1/models/Ticket.java
+++ src/main/java/org/revature/p1/models/Ticket.java
@@ -1,24 +1,153 @@
 package org.revature.p1.models;
 
 import org.revature.p1.utils.enums.TicketStatus;
-import org.revature.p1.utils.exceptions.TicketInvalidAmountException;
+import org.revature.p1.utils.enums.TicketType;
+
+import java.util.Arrays;
 
 public class Ticket {
-    private TicketStatus status;
+    private String id;
+    private User issuer;
+    private long submitDate;
+    private User resolver;
+    private long resolveDate;
     private String description;
-    private float amount;
+    private double amount;
+    private byte [] receipt;
+    private String paymentId;
+    private TicketStatus status;
+    private TicketType type;
 
-    public Ticket(TicketStatus status, String description, float amount) throws TicketInvalidAmountException {
-        if (amount < 0) {
-            throw new TicketInvalidAmountException("test");
-        }
+    public Ticket() {
+        super();
+    }
 
-        this.status = status;
+    public Ticket(String id,
+                  User issuer,
+                  long submitDate,
+                  User resolver,
+                  long resolveDate,
+                  String description,
+                  double amount,
+                  byte[] receipt,
+                  String paymentId,
+                  TicketStatus status,
+                  TicketType type) {
+        this.id = id;
+        this.issuer = issuer;
+        this.submitDate = submitDate;
+        this.resolver = resolver;
+        this.resolveDate = resolveDate;
         this.description = description;
+        this.amount = amount;
+        this.receipt = receipt;
+        this.paymentId = paymentId;
+        this.status = status;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public User getIssuer() {
+        return issuer;
+    }
+
+    public long getSubmitDate() {
+        return submitDate;
+    }
+
+    public User getResolver() {
+        return resolver;
+    }
+
+    public long getResolveDate() {
+        return resolveDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public byte[] getReceipt() {
+        return receipt;
+    }
+
+    public String getPaymentId() {
+        return paymentId;
+    }
+
+    public TicketStatus getStatus() {
+        return status;
+    }
+
+    public TicketType getType() {
+        return type;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setIssuer(User issuer) {
+        this.issuer = issuer;
+    }
+
+    public void setSubmitDate(long submitDate) {
+        this.submitDate = submitDate;
+    }
+
+    public void setResolver(User resolver) {
+        this.resolver = resolver;
+    }
+
+    public void setResolveDate(long resolveDate) {
+        this.resolveDate = resolveDate;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAmount(double amount) {
         this.amount = amount;
     }
 
-    public Ticket() throws TicketInvalidAmountException {
-        this(TicketStatus.PENDING, "", 0.0f);
+    public void setReceipt(byte[] receipt) {
+        this.receipt = receipt;
+    }
+
+    public void setPaymentId(String paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public void setStatus(TicketStatus status) {
+        this.status = status;
+    }
+
+    public void setType(TicketType type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Ticket{" +
+                "id='" + id + '\'' +
+                ", issuer=" + issuer +
+                ", submitDate=" + submitDate +
+                ", resolver=" + resolver +
+                ", resolveDate=" + resolveDate +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                ", receipt=" + Arrays.toString(receipt) +
+                ", paymentId='" + paymentId + '\'' +
+                ", status=" + status +
+                ", type=" + type +
+                '}';
     }
 }
--- src/main/java/org/revature/p1/models/User.java
+++ src/main/java/org/revature/p1/models/User.java
@@ -1,7 +1,114 @@
 package org.revature.p1.models;
 
-public class User {
-    public User() {
+import org.revature.p1.utils.enums.ClientUserType;
 
+public class User {
+    private String id;
+    private String username;
+    private String password;
+    private String email;
+    private String forename;
+    private String surname;
+    private boolean active;
+    private ClientUserType role;
+
+    public User() {
+        super();
+    }
+
+    public User(String id,
+                String username,
+                String password,
+                String email,
+                String forename,
+                String surname,
+                boolean active,
+                ClientUserType role) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.forename = forename;
+        this.surname = surname;
+        this.active = active;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getForename() {
+        return forename;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public ClientUserType getRole() {
+        return role;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setForename(String forename) {
+        this.forename = forename;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public void setRole(ClientUserType role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", forename='" + forename + '\'' +
+                ", surname='" + surname + '\'' +
+                ", active=" + active +
+                ", role=" + role +
+                '}';
     }
 }
