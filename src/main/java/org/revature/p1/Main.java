384b7d47879c7099b8fd39e9829e442eee17773f
Yuan Cao <yuancao85@gmail.com>
2022-11-30 01:52:57 -0500

    refactor to make use of the router

--- src/main/java/org/revature/p1/Main.java
+++ src/main/java/org/revature/p1/Main.java
@@ -1,36 +1,16 @@
 package org.revature.p1;
 
-import org.revature.p1.models.Ticket;
-import org.revature.p1.utils.exceptions.TicketInvalidAmountException;
+import io.javalin.Javalin;
+import org.revature.p1.utils.Router;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import static io.javalin.apibuilder.ApiBuilder.*;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Ticket> tickets = new ArrayList<>();
+        Javalin app = Javalin.create(c -> {
+            c.contextPath = "/reimbursement_system";
+        }).start(8080);
 
-        while(true) {
-            System.out.println("Type stop to terminate the program; type size to see how many tickets have been entered.");
-            System.out.println("Add another ticket? (type stop to terminate program)");
-            Scanner scan = new Scanner(System.in);
-
-            String line = scan.nextLine();
-            switch(line) {
-                case "size":
-                    System.out.println("# of tickets entered: " + Integer.toString(tickets.size()));
-                    break;
-                case "stop":
-                    System.exit(0);
-                    break;
-                default:
-                    try {
-                        Ticket ticket = new Ticket();
-                        tickets.add(ticket);
-                    } catch (TicketInvalidAmountException e) {
-                        System.out.println("The reimbursement amount sought cannot be negative.");
-                    }
-            }
-        }
+        Router.route(app);
     }
 }
