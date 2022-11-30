26c35f15708ebd997fa3267e1f36405f8798dec3
Yuan Cao <yuancao85@gmail.com>
2022-11-30 01:57:29 -0500

    create enums for the types of users we can have in the ers system

--- src/main/java/org/revature/p1/utils/enums/ClientUserType.java
+++ src/main/java/org/revature/p1/utils/enums/ClientUserType.java
@@ -2,8 +2,6 @@ package org.revature.p1.utils.enums;
 
 public enum ClientUserType {
     ADMIN,
-    ACTIVE_MANAGER,
-    DEACTIVATED_MANAGER,
-    ACTIVE_EMPLOYEE,
-    DEACTIVATED_EMPLOYEE
+    EMPLOYEE,
+    MANAGER
 }
