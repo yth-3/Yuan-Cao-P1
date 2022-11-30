f9110c6b5642a620c5fea59e9979c08ea5f4104c
Yuan Cao <yuancao85@gmail.com>
2022-11-30 01:54:13 -0500

    create sub that fit the new interface

--- src/main/java/org/revature/p1/daos/TicketDao.java
+++ src/main/java/org/revature/p1/daos/TicketDao.java
@@ -1,4 +1,32 @@
 package org.revature.p1.daos;
 
-public class TicketDao {
+import org.revature.p1.models.Ticket;
+
+import java.util.List;
+
+public class TicketDao implements CrudDao<Ticket> {
+    @Override
+    public void create(Ticket obj) {
+
+    }
+
+    @Override
+    public Ticket read(Ticket obj) {
+        return null;
+    }
+
+    @Override
+    public void update(Ticket obj) {
+
+    }
+
+    @Override
+    public void delete(Ticket obj) {
+
+    }
+
+    @Override
+    public List<Ticket> findAll() {
+        return null;
+    }
 }
